9414454bd2781b70bc621a42626d037191ef9e34
BPR <benjamin.portier@econocom.com>
2020-01-20 10:14:50 +0100

    UPD: Add Log Level stage and VERBOSE param

--- pipeline_jenkins/install_wordpress.groovy
+++ pipeline_jenkins/install_wordpress.groovy
@@ -6,7 +6,24 @@ pipeline {
     ansiColor("xterm")
   }
 
+  parameters {
+    choice(name: 'VERBOSE', choices: ['Yes', "No"], description: "Choice Yes to have a verbose mode on logs")
+    choice(name: 'ansible_environment', choices: ['DEV, INT, PROD'], description: 'Environment where you want to play your pipeline')
+  }
+
   stages {
+
+    stage("Log level") {
+      steps {
+        script {
+          if (VERBOSE == 'Yes') {
+              LOG_LEVEL = '-vvvv'
+          } else {
+              LOG_LEVEL = ''
+          }
+        }
+      }
+    }
 
     // stage ('Clonage repo GIT') {
     //   steps {
