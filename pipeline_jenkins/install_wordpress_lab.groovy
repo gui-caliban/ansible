476686f2898077fb7d241a78cff802d2f704385f
Guillaume Ferrari <guillaume.ferrari@econocom.com>
2020-01-21 09:45:59 +0100

    Add parm on pipeline

--- pipeline_jenkins/install_wordpress_lab.groovy
+++ pipeline_jenkins/install_wordpress_lab.groovy
@@ -6,33 +6,39 @@ pipeline {
 		ansiColor("xterm")
 	}
 
-stages {
+  parameters {
+    choice(name: 'VERBOSE', choices: [' ', "-v", "-vv", "-vvv", "-vvvv"], description: "Choice verbosity on logs")
+    choice(name: 'ansible_environment', choices: ['INT', 'DEV', 'PROD'], description: 'Environment where you want to play your pipeline')
+    choice(name: 'GIT_BRANCH', choices: ['develop', 'feature/CentOS'], description: 'Git branch you want to play')
+  }
 
-		stage ('Clonage repo GIT') {
-			steps {
-				checkout([
-          $class: 'GitSCM',
-          branches: [[name: '*/feature/creation_cours']],
-          doGenerateSubmoduleConfigurations: false,
-          extensions: [],
-          submoduleCfg: [],
-          userRemoteConfigs: [[credentialsId: 'd35a7e7a-3b40-4772-a2a3-cc2795010ce1', url: 'https://github.com/gui-caliban/ansible.git']]
-        ])
-			}
-    }
+  stages {
 
-    stage ('Invoke Ansible Playbook install_wordpress.yml') {
-      environment {
-        ANSIBLE_FORCE_COLOR = true
+  		stage ('Clonage repo GIT') {
+  			steps {
+  				checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/${GIT_BRANCH}']],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [],
+            submoduleCfg: [],
+            userRemoteConfigs: [[credentialsId: 'd35a7e7a-3b40-4772-a2a3-cc2795010ce1', url: 'https://github.com/gui-caliban/ansible.git']]
+          ])
+  			}
       }
-      steps {
-        ansiblePlaybook (
-          colorized: true,
-          playbook: 'install_wordpress.yml',
-          inventory: 'inventories/INT/hosts',
-          extras: '${VERBOSE}'
-        )
+
+      stage ('Invoke Ansible Playbook install_wordpress.yml') {
+        environment {
+          ANSIBLE_FORCE_COLOR = true
+        }
+        steps {
+          ansiblePlaybook (
+            colorized: true,
+            playbook: 'install_wordpress.yml',
+            inventory: 'inventories/${ansible_environment}/hosts',
+            extras: '${VERBOSE}'
+          )
+        }
       }
     }
   }
-}
