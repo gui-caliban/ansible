bcf66b05c09503687eb76a2ca4f02a1a156f0dfb
Guillaume Ferrari <guillaume.ferrari@econocom.com>
2020-01-21 10:23:54 +0100

    up

--- pipeline_jenkins/install_wordpress_lab.groovy
+++ pipeline_jenkins/install_wordpress_lab.groovy
@@ -8,7 +8,7 @@ pipeline {
 
   parameters {
     choice(name: 'VERBOSE', choices: [' ', "-v", "-vv", "-vvv", "-vvvv"], description: "Choice verbosity on logs")
-    choice(name: 'ansible_environment', choices: ['INT', 'DEV', 'PROD'], description: 'Environment where you want to play your pipeline')
+    choice(name: 'ansible_environment', choices: ['INT_Ubuntu', 'INT_CentOS'], description: 'Environment where you want to play your pipeline')
   }
 
   stages {
